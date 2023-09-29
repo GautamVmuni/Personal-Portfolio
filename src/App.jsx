001e71b4535750e6e78b501731c228988083fdd0
Gautam Muni <gautam.muni.13837@gmail.com>
2023-09-29 19:57:24 +0530

    Created pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Portfolio from './Pages/Portfolio/Portfolio'
+import Contact from './Pages/Contact/Contact'
+
 function App() {
   return (
     <></>
