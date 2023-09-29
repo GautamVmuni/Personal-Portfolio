74f2dcbf766075a99f192c2b09f55a01ebf94142
Gautam Muni <gautam.muni.13837@gmail.com>
2023-09-29 21:14:01 +0530

    created NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import Navbar from './Components/Navbar'
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { links } from '../data';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="nav">
+      <div className="nev_menu">
+        <ul className="nav_list">
+          {links.map(({name, icon, path}, index) => {
+            return (
+              <li className="nav_item" key={index}>
+                <NavLink to={path} className="nav_link">
+                  {icon}
+                  <h3 className="nav_name">{name}</h3>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="nav_toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
   )
 }
 
