12f3f3ef6f43871e5a9e8f76a6e6b9734564d74f
Gautam Muni <gautam.muni.13837@gmail.com>
2023-09-29 22:57:58 +0530

    Done with Home Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import ProfileImage from '../../assets/home.jpg';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home section grid">
+      <img src={ProfileImage} alt="" className="home_img" />
+
+      <div className="home_content">
+        <div className="home_data">
+          <h1 className="home_title">
+            <span>I'm Gautam Muni.</span> Software Developer
+          </h1>
+
+          <p className="home_description">
+            A passionate and creative software developer with a 
+            penchant for turning ideas into reality through code. 
+            Welcome to my digital space, where I showcase my journey, 
+            skills, and the projects that make up my coding odyssey.
+          </p>
+
+          <Link to='/about' className='button'>
+            More About Me {' '}
+            <span className="button_icon">
+              <FaArrowRight /> 
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color_block"></div>
+    </section>
   )
 }
 
